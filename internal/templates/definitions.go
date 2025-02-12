860d6784a383f30ed51c2fbf205c4c518f64455c
Aaron Vaz <vazaaron08@gmail.com>
2025-02-12 19:44:59 +0000

    fix template definition

--- internal/templates/definitions.go
+++ internal/templates/definitions.go
@@ -1,24 +1,15 @@
 package templates
 
-type Data struct {
-	Name string `yaml:"name"`
-	Type string `yaml:"type"`
-}
-
 type Input struct {
-	Data
-	Default any `yaml:"default"`
-}
-
-type Env struct {
-	Data
-	Value any `yaml:"value"`
+	Name        string `yaml:"name"`
+	Description string `yaml:"description"`
+	Default     any    `yaml:"default,omitempty"`
 }
 
 type ProjectTemplate struct {
-	Name        string  `yaml:"name"`
-	Description string  `yaml:"description"`
-	Renderer    string  `yaml:"render"`
-	Inputs      []Input `yaml:"inputs"`
-	Env         []Env   `yaml:"env"`
+	Name        string         `yaml:"name"`
+	Description string         `yaml:"description"`
+	Renderer    string         `yaml:"renderer,omitempty"`
+	Inputs      []Input        `yaml:"inputs"`
+	Env         map[string]any `yaml:"env"`
 }
